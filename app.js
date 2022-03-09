d5452f8d3de682f920925fdf8568273575dac58b
@haroon11 <haroon@netart.io>
2022-03-09 20:44:36 +0530

    Minor Edits

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const Express = require('express')
 const app = Express()
 const RouteHandling = require('./RouteHandling')
-const portNumber = process.env.PORT_NUMBER || process.argv[2] || 8000
+const portNumber =
+  process.env.PORT_NUMBER || process.argv[2] || process.env.PORT || 8000
 const dbConnection = require('./mongoDB/DBConnection')
 const db = dbConnection.connection
 
@@ -14,7 +15,7 @@ app.use(RouteHandling)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.listen(portNumber, () => {
-  console.log('listening at 8000')
+  console.log(`Listening at ${portNumber}`)
 })
 
 module.exports = app
